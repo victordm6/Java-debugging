f57fc5c274165709ca7dce7cdfdbe78e028221ed
victordm6 <victor.dm6@hotmail.com>
2022-07-01 08:29:31 -0300

    :tada: Commit inicial

---
+++ src/CalcularMedias.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CalcularMedias {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        String[] alunos = {"Camila", "Lucas", "Bruna", "Pedro"};
+
+        double media = calculaMediaDaTurma(alunos, teclado);
+
+        System.out.printf("Média da turma %.1f", media);
+    }
+
+    public static double calculaMediaDaTurma(String[] alunos, Scanner scanner) {
+
+        double soma = 0;
+
+        for(String aluno : alunos) {
+            System.out.printf("Nota do aluno %s: ", aluno);
+            double nota = scanner.nextDouble();
+            soma += nota;
+        }
+        return soma / alunos.length;
+    }
+
+}
